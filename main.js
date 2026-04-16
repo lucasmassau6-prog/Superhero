1e9c73f856a26d9bbb70a54847802f3523e16b9d
lucasmassau6-prog <lucasmassau6@gmail.com>
2026-04-15 23:23:11 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,42 @@
-const BaseURL = 'https://superheroapi.com/api/'+ '9fe283d9374dc764c879de3efbf856fa';
+const BASE_URL = 'https://superheroapi.com/api.php/';
+const API_KEY = '9fe283d9374dc764c879de3efbf856fa';
 
-var url = BaseURL + "/69";
+var minhasCartas = [12, 33, 44]
+var selecionada = 12;
 
-callAPI(url, function(status, response){
-    console.log(response);
-})
+window.onload = function(){
+    this.getAndShowHero(getRandom);
+}
+
+function getRandom(){
+    return Math.floor(Math.random() * 731) + 1;
+}
+
+function getAndShowHero(id){
+    var url = BASE_URL + "/" + API_KEY + "/" + id;
+
+    callAPI(url, function(status, data){
+        let name = data.name;
+        let intelligence = data.powerstats.intelligence;
+        let strength = data.powerstats.strength;
+        let speed = data.powerstats.speed;
+        let durability = data.powerstats.durability;
+        let power = data.powerstats.power;
+        let combat = data.powerstats.combat;
+        let image = data.image.url;
+
+        document.getElementById("content").innerHTML += "<article> <img src='" + image + "'/>"+
+        "<h1>" + name + "</h1>" +
+        "<p>Intelligence: <span style='width:" + intelligence + "%; background-color: #F9B32F'></span></p>" +
+        "<p>Strength:     <span style='width:" + strength + "%; background-color: #FF76CC'></span></p>" +
+        "<p>Speed:        <span style='width:" + speed + "%; background-color: #22A7F0'></span></p>" +
+        "<p>Durability:   <span style='width:" + durability + "%; background-color: #3EDC81'></span></p>" +
+        "<p>Power:        <span style='width:" + power + "%; background-color: #AB69C6'></span></p>" +
+        "<p>Combat:       <span style='width:" + combat + "%; background-color: #9CAAB9'></span></p>" +
+        "</article>";
+    });
+}
+
 function callAPI(url, callback){
     var xhr = new XMLHttpRequest();
     xhr.open("GET", url, true);
